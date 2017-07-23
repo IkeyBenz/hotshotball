52ecbeadf1a8279556d0bb1a484ce897795941f9
Ikey Benzaken <ikey.benz@gmail.com>
2017-07-23 16:15:13 -0400

    Optimized venue images and fixed bug in header images

--- functions.js
+++ functions.js
@@ -29,8 +29,8 @@ $(document).ready(function(){
       if (op <= 0.1) {
           element.style.display = 'none';
           clearInterval(timer);
-          if (index < 22) {index += 1}
-          else if (index == 22) {index = 1}
+          if (index < 20) {index += 1}
+          else if (index == 20) {index = 1}
           element.src = "Images/HEADERIMAGES/scrollingPic" + index + ".JPG";
 
       }
@@ -68,7 +68,9 @@ function closeVideo() {
   document.getElementById('closeButton').text = "";
 }
 var venueLeft = 0;
+
 function moveVenueLeft() {
+  var venueFigure = document.getElementById('venueFigure');
   document.getElementById('venueLastBtn').style.display = "";
   venueLeft -= (100 / 3);
   if (venueLeft == -(100 / 3)) {
@@ -76,39 +78,43 @@ function moveVenueLeft() {
   } else if (venueLeft == -(200 / 3)) {
     document.getElementById('venueCaption').innerHTML = "CUSTOM WOODEN BACKBAORDS";
   }
-  document.getElementById('venueFigure').style.WebkitTransform = "translatex("+venueLeft+"%)";
-  document.getElementById('venueFigure').style.msTransform = "translatex("+venueLeft+"%)";
-  document.getElementById('venueFigure').style.transform = "translatex("+venueLeft+"%)";
+  venueFigure.style.WebkitTransform = "translatex("+venueLeft+"%)";
+  venueFigure.style.msTransform = "translatex("+venueLeft+"%)";
+  venueFigure.style.transform = "translatex("+venueLeft+"%)";
   if (venueLeft <= -65) {document.getElementById('venueNextBtn').style.display = "none";}
   return false;
 }
 function moveVenueRight() {
+  var venueFigure = document.getElementById('venueFigure');
   venueLeft += (100 / 3);
   if (venueLeft == -(100 / 3)) {document.getElementById('venueCaption').innerHTML = "ROBUST GOALS"}
   else if (venueLeft == 0) {document.getElementById('venueCaption').innerHTML = "HOT SHOT SIGNATURE COURT"}
   document.getElementById('venueNextBtn').style.display = "";
-  document.getElementById('venueFigure').style.WebkitTransform = "translatex("+venueLeft+"%)";
-  document.getElementById('venueFigure').style.msTransform = "translatex("+venueLeft+"%)";
-  document.getElementById('venueFigure').style.transform = "translatex("+venueLeft+"%)";
+  venueFigure.style.WebkitTransform = "translatex("+venueLeft+"%)";
+  venueFigure.style.msTransform = "translatex("+venueLeft+"%)";
+  venueFigure.style.transform = "translatex("+venueLeft+"%)";
   if (venueLeft == 0) {document.getElementById('venueLastBtn').style.display = "none"}
   return false;
 }
 var teamLeft = 0;
+
 function slideTeamLeft() {
+  var teamFigure = document.getElementById('teamFigure');
   document.getElementById('teamLastBtn').style.display = "";
   teamLeft -= (100 / 9);
-  document.getElementById('teamFigure').style.WebkitTransform = "translatex("+teamLeft+"%)";
-  document.getElementById('teamFigure').style.msTransform = "translatex("+teamLeft+"%)";
-  document.getElementById('teamFigure').style.transform = "translatex("+teamLeft+"%)";
-  if (teamLeft <= -87) {document.getElementById('teamNextBtn').style.display = "none"}
+  teamFigure.style.WebkitTransform = "translatex("+teamLeft+"%)";
+  teamFigure.style.msTransform = "translatex("+teamLeft+"%)";
+  teamFigure.style.transform = "translatex("+teamLeft+"%)";
+  if (teamLeft <= -(800 / 9)) {document.getElementById('teamNextBtn').style.display = "none"}
   return false;
 }
 function slideTeamRight() {
+  var teamFigure = document.getElementById('teamFigure');
   document.getElementById('teamNextBtn').style.display = "";
   teamLeft += (100 / 9);
-  document.getElementById('teamFigure').style.WebkitTransform = "translatex("+teamLeft+"%)";
-  document.getElementById('teamFigure').style.msTransform = "translatex("+teamLeft+"%)";
-  document.getElementById('teamFigure').style.transform = "translatex("+teamLeft+"%)";
+  teamFigure.style.WebkitTransform = "translatex("+teamLeft+"%)";
+  teamFigure.style.msTransform = "translatex("+teamLeft+"%)";
+  teamFigure.style.transform = "translatex("+teamLeft+"%)";
   if (teamLeft == 0) {document.getElementById('teamLastBtn').style.display = "none"}
   return false;
 }
