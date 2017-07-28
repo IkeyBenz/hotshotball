17aaf8e4a7d544a7fefc9a164ca9075f1a347f5b
Ikey Benzaken <ikey.benz@gmail.com>
2017-07-27 22:28:44 -0400

    Bad

---
+++ server.php
@@ -0,0 +1,15 @@
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+\Stripe\Stripe::setApiKey("sk_test_UvSGUatwMyYXsn8jbqWsfvov");
+
+// Token is created using Stripe.js or Checkout!
+// Get the payment token submitted by the form:
+$token = $_POST['stripeToken'];
+
+// Charge the user's card:
+$charge = \Stripe\Charge::create(array(
+  "amount" => 1000,
+  "currency" => "usd",
+  "description" => "Example charge",
+  "source" => $token,
+));
